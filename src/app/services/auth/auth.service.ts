88e1985924db1224c9248defa4771a7e482a58c3
FCT-Marcos <cuentaboost12@gmail.com>
2024-04-19 14:27:07 +0100

    Created auth service for handle auth

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, catchError, throwError, tap, map } from 'rxjs';
+import { LoginRequest } from '../../interfaces/LoginRequest';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { RegisterRequest } from '../../interfaces/RegisterRequest';
+import { Router } from '@angular/router';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+
+  isUserLogged: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  userToken: BehaviorSubject<string> = new BehaviorSubject<string>("");
+
+  constructor(private http: HttpClient, private router:Router) {
+    this.isUserLogged = new BehaviorSubject<boolean>(sessionStorage.getItem("access_token") != null);
+    this.userToken = new BehaviorSubject<string>(sessionStorage.getItem("access_token") || "");
+  }
+
+  login(credentials:LoginRequest): Observable<any> {
+    return this.http.post<any>(`${environment.API_URL_AUTH}/login`, credentials, httpOptions).pipe(
+      tap((userData) => {
+        sessionStorage.setItem("access_token", userData.token);
+        this.userToken.next(userData.token);
+        this.isUserLogged.next(true);
+      }),
+      map((userData)=> userData.token),
+      catchError(this.handleError)
+    );
+  }
+
+  register(credentials:RegisterRequest): Observable<any> {
+    return this.http.post<any>(`${environment.API_URL_AUTH}/register`, credentials, httpOptions).pipe(
+      tap((userData) => {
+        sessionStorage.setItem("access_token", userData.token);
+        this.userToken.next(userData.token);
+        this.isUserLogged.next(true);
+      }),
+      map((userData)=> userData.token),
+      catchError(this.handleError)
+    );
+  }
+
+  logout(): void {
+    sessionStorage.removeItem("access_token");
+    this.isUserLogged.next(false)
+    this.router.navigateByUrl("/login")
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error("Se ha producido un error ", error.error)
+    }else{
+      console.error("Error status code ", error.status)
+    }
+
+    return throwError(() => new Error("Algo falló, Por favor intentelo de nuevo"))
+  }
+
+}
