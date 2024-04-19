09fb68777124d064f984aad47a2996aeb0605eb4
FCT-Marcos <cuentaboost12@gmail.com>
2024-04-19 14:26:24 +0100

    Added header component and transactions table to home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { TransactionsTableComponent } from '../../components/transactions-table/transactions-table.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, HeaderComponent, TransactionsTableComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
