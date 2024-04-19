1cc5b7b4e48e19c2096fef55393638de58be50bb
FCT-Marcos <cuentaboost12@gmail.com>
2024-04-19 14:27:15 +0100

    env files

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    PAGE_TITLE: "Bank App",
+    API_URL_AUTH: 'http://localhost:8080/api/v1/auth',
+    API_URL: 'http://localhost:8080/api/v1',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    PAGE_TITLE: "Bank App",
+    API_URL_AUTH: 'http://localhost:8080/api/v1/auth',
+    API_URL: 'http://localhost:8080/api/v1',
+};
