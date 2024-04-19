1ca16a3416eca98d129c67031d06d017b689187c
FCT-Marcos <cuentaboost12@gmail.com>
2024-04-19 14:24:22 +0100

    Added routes and guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { HomeComponent } from './pages/home/home.component';
+import { authGuard } from './guards/authGuard/auth.guard';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 export const routes: Routes = [
     {path: "", redirectTo: "login", pathMatch: "full"},
     {path: "login", component: LoginComponent},
-    {path: "home", component: HomeComponent},
+    {path: "home", component: HomeComponent, canActivate: [authGuard]},
+    {path: "profile", component: ProfileComponent, canActivate: [authGuard]},
 ];
